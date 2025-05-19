b8082d15ed460f3c68f9fd33cb4d980296bc178e
Abel Mahón Cortés <Amahoncortes@gmail.com>
2025-05-19 19:56:27 +0200

    Adicion_tabla_proyectos

--- src/db/init.js
+++ src/db/init.js
@@ -1,5 +1,6 @@
-const db = require('./database');
+const db = require("./database");
 
+//Tabla de usuarios
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS users (
@@ -8,4 +9,13 @@ db.serialize(() => {
       password TEXT NOT NULL
     )
   `);
+
+  //Tabla de proyectos
+  db.run(`CREATE TABLE IF NOT EXISTS projects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  )`);
 });
